56266583a9abf52a291a6510f0feaba4f782a059
Abson Dutra Galvão <87133277+AbsonDutraGalvao@users.noreply.github.com>
2023-11-28 16:45:07 -0300

    style: create new layout home page

--- lib/pages/home_page/home_page.dart
+++ lib/pages/home_page/home_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:tdd_flutter/data/task_data.dart';
+
+import '../../model/task_model.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,8 +11,52 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  List<TaskModel>  tasks = TaskData.listTasks;
+  TextEditingController taskNameController = TextEditingController();
+  GlobalKey<FormState> _key = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Lista de Tarefas"),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+                itemCount: tasks.length,
+                itemBuilder: (context, index){
+                  return ListTile(
+                    title: Text(tasks[index].name,style: TextStyle(color: Colors.black),),
+                  );
+                }),
+          ),
+          Form(
+            key: _key,
+            child: TextFormField(
+              controller: taskNameController,
+              validator: (value){
+                if(value?.isEmpty ?? true){
+                  return "Esse carmpo precisa ser preenchido";
+                }
+              },
+            ),
+          ),
+          ElevatedButton(onPressed: (){
+            if(!_key.currentState!.validate()){
+              return;
+            }
+            setState(() {
+              tasks.add(TaskModel(name: taskNameController.text, check: false));
+            });
+            var snackBar = SnackBar(content: Text('Sua Tarefa ${taskNameController.text} foi criada com sucesso'));
+            ScaffoldMessenger.of(context).showSnackBar(snackBar);
+            taskNameController.clear();
+          }, child: Text("Criar Tarefa"))
+        ],
+      ),
+    );
   }
 }
